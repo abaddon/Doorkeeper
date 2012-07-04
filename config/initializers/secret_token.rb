b27b086449b607425e7a7e93bcb89683b3725c16
Stefano Longhi <stefano@moneyfarm.com>
2012-07-04 21:27:19 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorkeeper::Application.config.secret_token = '29e283102fe53bffa2f459ade9ebe4b80b8df4f3a8682f8d394a442a55fbaea67e4b99e96b7807640fc4494081b244a641c7cabc86c1cc37b6cdc5b6fa9a82da'
